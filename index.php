0f457c3848569376f0f10fefc97fb56a95c92019
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-09-24 17:38:30 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Fathomless Caverns of Peril</title>
+    </head>
+    <body>
+        <h1>Welcome To Fathomless Cavers of Peril</h1>
+        <a href = "https://fathomless.io/test/">Test Python Code</a>
+        <a href = "https://fathomless.io/engine/">Game Engine</a>
+        <a href = "https://fathomless.io/game/">Demo Game</a>
+    </body>
+</html>
