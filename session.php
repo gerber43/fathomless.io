91e351997431bb288d5683a284af4582004e9ad6
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-09-28 02:36:03 -0400

    Update session.php
    Created session rule that required a valid session to be active to proceed

--- session.php
+++ session.php
@@ -1,4 +1,10 @@
 <?php session_start();
     $uuid = $_SESSION['uuid']??null;
     $username = $_SESSION['username']??null;
+    /*Only use session variables to create access rules*/
+    function session_required() {
+        if (!$_SESSION['uuid']) {
+            header("Location: https://fathomless.io/account/");
+        }
+    }
 ?>
