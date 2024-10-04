09d4bda55f7a885771a2db2abd2f7df0ada0d939
YuchuanHuang <huan1695@purdue.edu>
2024-10-04 18:58:44 -0400

    Update map_simplifier.py

--- cgi-bin/map_simplifier.py
+++ cgi-bin/map_simplifier.py
@@ -1,1 +1,14 @@
+import json
+
+
+def delete_blank_object(game_map):
+    for x, row in enumerate(game_map):
+        for y, tile in enumerate(row):
+            keys_to_delete = set()
+            for key, value in tile.items():
+                if isinstance(value, dict) and value.get('textureIndex') == 8:
+                    keys_to_delete.add(key)
+            for key in keys_to_delete:
+                del tile[key]
+    return game_map
 
