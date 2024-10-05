dd7dfbb72f57c320b7d6724894cacdd62ecd48af
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-05 17:53:46 -0400

    Update engine.php
    Created manhattan distance function, removed mobile buttons in support of tile touch interactions, created a more clear loader representation

--- engine.php
+++ engine.php
@@ -12,12 +12,19 @@
             margin:0;
             padding:0;
             touch-action: manipulation;
+            -webkit-touch-callout: none;
+            -webkit-user-select: none;
+            -khtml-user-select: none;
+            -moz-user-select: moz-none;
+            -ms-user-select: none;
+            -o-user-select: none;
+            user-select: none;
             }
             #page {
             width:100vw;
             height:100svh;
             }
-            #page,#canvas .space,#inventory,#inventory div {
+            #page,#canvas .tile,#inventory,#inventory div {
             display:flex;
             align-items:center;
             justify-content:center;
@@ -28,23 +35,7 @@
             #canvas {
             display: grid;
             }
-            #darkness {
-                background:#333;
-                position:absolute;
-                mix-blend-mode: hard-light;
-                display:flex;
-                align-items:center;
-                justify-content:center;
-                
-            }
-            #darkness #hole {
-                position:absolute;
-                width: 60%;
-	            height: 60%;
-	            border-radius:50%;
-            	background-color: gray;
-            }
-            .space div {
+            .tile div {
             position:absolute;
             background-repeat: no-repeat;  
             background-size: cover;
@@ -180,51 +171,35 @@
             margin:0;
             font-size:inherit;
             }
-            #mobile_movement {
-            position:absolute;
-            bottom:0;
-            right:0;
-            display: none;
-            grid-template-columns: repeat(3, 1fr);
-            grid-template-rows: repeat(3, 1fr);
-            grid-column-gap: 0px;
-            grid-row-gap: 0px;
-            opacity:.5;
-            z-index:1;
-            }
-            #mobile_movement button {
-            width:100px;
-            height:100px;
-            }
-            #mobile_movement button:nth-child(1) {grid-area: 1 / 2 / 2 / 3;}
-            #mobile_movement button:nth-child(2) {grid-area: 2 / 1 / 3 / 2;}
-            #mobile_movement button:nth-child(4) {grid-area: 3 / 2 / 4 / 3;}
-            #mobile_movement button:nth-child(3) {grid-area: 2 / 3 / 3 / 4;}
             button, input {
             transition:.75s;
-            background: none;
-            color: inherit;
-            border: none;
-            padding: 0;
-            font: inherit;
-            cursor: pointer;
-            outline: inherit;
-            color:gold;
-            font-size:20px;
-            border:2px burlywood solid;
-            background:saddlebrown;
-            background:url(https://img.freepik.com/premium-vector/seamless-pattern-old-wood-wall-background_117579-47.jpg);
             }
             button, input[type=submit]:hover {
             transition:.75s;
             transform:scale(.9);
+            }
+            .manhattan {
+                background: purple;
+                opacity:.3;
+            }
+            .tile[data-manhattan="1"]:hover .light{
+                background: purple;
+                opacity:.3;
+                cursor:pointer;
+            }
+            #cover {
+              position:absolute;
+              top:0;
+              left:0;
+              width:100vw;
+              height:100vh;
+              cursor:wait;
             }
         </style>
     </head>
     <body onresize="scaleTextures()">
         <div id = "page">
             <div id = "canvas"></div>
-            <div id = "darkness"><div id = "hole"></div></div>
         </div>
         <div id = "dialogue"></div>
         <div id = "alert"></div>
@@ -235,15 +210,8 @@
                 </button>
             <span id = "settings_span">
                 <button data-setting = "toggleAudio">Play SFX</button>
-                <button data-setting = "onScreenControls">On-screen Controls</button>
-                <button data-setting = "toggleAscii">Enable Ascii</button><button>Setting 4</button>
+                <button data-setting = "toggleAscii">Enable Ascii</button>
             </span>
-        </div>
-        <div id = "mobile_movement">
-            <button data-direction = "ArrowUp">Up</button>
-            <button data-direction = "ArrowLeft">Left</button>
-            <button data-direction = "ArrowRight">Right</button>
-            <button data-direction = "ArrowDown">Down</button>
         </div>
         <script>
             const tileObjects = JSON.parse('<?=file_get_contents("https://fathomless.io/json/objects.json")?>');
@@ -251,16 +219,9 @@
             const arrowKeys = ["ArrowRight","ArrowDown","ArrowLeft","ArrowUp","KeyD","KeyS","KeyA","KeyW"];
             const defaults = {"terrain":{"textureIndex":3,"rotation":0},"light":{"textureIndex":8,"rotation":0,"intensity":0},"default":{"textureIndex":8,"rotation":0}};
             var playerInventory = JSON.parse('<?=file_get_contents("https://fathomless.io/json/inventory.json");?>');
-            var start = objectTypes = disableMovement = onScreenControls = playAudio = closeSetting = isSettingsOpen = currentMap = viewDiameter = levelStep = inventoryOpened = textInputOpened = isEditMap = editRotation = previousPortal=asciiMode= 0;
+            var start = disableMovement = playAudio = closeSetting = isSettingsOpen = currentMap = viewDiameter = levelStep = inventoryOpened = textInputOpened = isEditMap = editRotation = previousPortal=asciiMode= 0;
+            var objectTypes = ["terrain","item","decor","entity","light"];
             sendDirection();
-            isMobile()?toggleOnScreenControls():"";
-            function isMobile() {
-                return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-            }
-            function toggleOnScreenControls() {
-                document.getElementById('mobile_movement').style.display = onScreenControls?"none":"grid";
-                onScreenControls = !onScreenControls;
-            }
             function toggleSettings() {
                 document.getElementById('settings').style.width=(isSettingsOpen?"0px":"100vw");
                 document.getElementById('settings').style.fontSize=(isSettingsOpen?"0px":"20px");
@@ -272,15 +233,33 @@
                 document.getElementById('canvas').innerHTML = "";  
                 for (var i = 0; i < viewDiameter; i++) {
                     for (var j = 0; j < viewDiameter; j++) {
-                        var space = Object.assign(document.createElement('div'),{id:(j+","+i),classList:"space"})
-                        objectTypes.forEach((object) => {space.appendChild(Object.assign(document.createElement('div'),{classList:object}))});
-                        document.getElementById('canvas').appendChild(space);
+                        var tile = Object.assign(document.createElement('div'),{id:(j+","+i),classList:"tile"});
+                        tile.dataset.manhattan = Math.abs(j - Math.floor(viewDiameter/2)) + Math.abs(i - Math.floor(viewDiameter/2));
+                        objectTypes.forEach((object) => {tile.appendChild(Object.assign(document.createElement('div'),{classList:object}))});
+                        document.getElementById('canvas').appendChild(tile);
                     }
                 }
                 document.getElementById('dialogue').style.backgroundImage = 'url("'+tileObjects[12]['icon']+'")';
                 document.getElementById('inventory').style.backgroundImage = 'url("'+tileObjects[12]['icon']+'")';
                 document.getElementById('page').style.backgroundImage = 'url("'+tileObjects[13]['icon']+'")';
                 scaleTextures();
+                document.getElementById("canvas").querySelectorAll('.tile').forEach((item) => {
+                    item.addEventListener("click", () => {clickTile(item.id)});
+                    item.addEventListener("contextmenu", function(ev){ev.preventDefault();alert(item)});
+                });
+            }
+            function clickTile(tileId) {
+                var coordinates = tileId.split(",");
+                if (document.getElementById(tileId).dataset.manhattan == 1) {
+                    var index = Math.atan2(coordinates[1] - Math.floor(viewDiameter/2),coordinates[0] - Math.floor(viewDiameter/2))*2/Math.PI
+                    directionHandler(arrowKeys[(index != -1)?index:3])
+                }
+            }
+            function displayManhattan(distance) {
+                document.getElementById('canvas').querySelectorAll('.manhattan').forEach((object) => {object.classList.remove("manhattan")});
+                for (var i = 1; i <= distance; i++){
+                    document.getElementById('canvas').querySelectorAll(`[data-manhattan*="`+i+`"]`).forEach((object) => {object.querySelector('.light').classList.add("manhattan")});
+                }
             }
             function updateMap() {
                 for (var i = 0; i < viewDiameter; i++) {
@@ -299,13 +278,8 @@
                 selectedElement.style.backgroundImage = 'url("'+tileObjects[object['textureIndex']][asciiMode?"ascii":"icon"]+'")';
                 selectedElement.style.transform = (type == "entity" && object['rotation'] == 180)?('scaleX(-1)'):('rotate('+object['rotation']+'deg)');
             }
-            function setDarkness(diameter,intensity) {
-                document.getElementById('hole').style.height = document.getElementById('hole').style.width = (100*diameter/viewDiameter)+"%";
-                document.getElementById('darkness').style.background = "#"+((Math.round(parseInt("0xFFFFFF", 16)*intensity/100)).toString(16)).padStart(6,"0");
-            }
             function scaleTextures() {
                 document.getElementById('canvas').style.height = document.getElementById('canvas').style.width = Math.min(window.innerWidth,window.innerHeight);
-                document.getElementById('darkness').style.height = document.getElementById('darkness').style.width = Math.min(window.innerWidth,window.innerHeight);
             }
             function uuidv4() {
                 return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>(+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16));
@@ -332,13 +306,11 @@
                 }
             }
             function sendDirection(direction = "") {
-                var loaderId = createMessage("alert","<img src = 'https://cdn.svgator.com/assets/landing-pages/static/css-loader/57579327-0-Loaders-3.svg'>",10);
                 var xmlhttp = new XMLHttpRequest();
                 xmlhttp.onreadystatechange = function () {
                     if (this.readyState == 4 && this.status == 200) {
                         var response = JSON.parse(this.responseText);
-                        if (response['map_subset'].length != viewDiameter || objectTypes != Object.keys(response['map_subset'][0][0])) {
-                            objectTypes = Object.keys(response['map_subset'][0][0]);
+                        if (response['map_subset'].length != viewDiameter) {
                             viewDiameter = response['map_subset'].length;
                             initializeMap();
                         }
@@ -346,8 +318,12 @@
                         updateMap();
                         createMessage("dialogue",response["message"],1);
                         disableMovement = false;
-                        document.getElementById(loaderId).remove();
                         console.log(`Time taken: `+(performance.now() - start)+` milliseconds`);
+                        displayManhattan(0);
+                        playSound(0);
+                        if (document.getElementById("cover")){
+                            document.getElementById("cover").remove()
+                        }
                     }
                   }
                   var queryString = (direction !== "")?"="+encodeURIComponent(direction):"";
@@ -356,9 +332,9 @@
             }
             function directionHandler(keycode) {
                 if (!disableMovement) {
+                    document.body.appendChild(Object.assign(document.createElement('div'),{id:("cover")}));
                     start = performance.now();
                     sendDirection((arrowKeys.indexOf(keycode)%4)*90);
-                    playSound(0);
                     disableMovement = true;
                 } 
             }
@@ -370,9 +346,7 @@
                     toggleInventory();
                 }
             });
-            document.getElementById('settings').addEventListener('mouseenter', (e) => {
-                clearTimeout(closeSetting);
-            });
+            document.getElementById('settings').addEventListener('mouseenter', (e) => {clearTimeout(closeSetting);});
             document.getElementById('settings').addEventListener('mouseleave', (e) => {
                 if (isSettingsOpen){
                     closeSetting = setTimeout(toggleSettings, 3000);
@@ -394,11 +368,9 @@
                         item.innerHTML = (!asciiMode)?"Enable Graphics":"Enable Ascii"
                         toggleAscii();
                     }
-                    if (item.dataset.setting && item.dataset.setting == "onScreenControls") {
-                        toggleOnScreenControls();
-                    }
                 });
             });
+          
         </script>
     </body>
 </html>
