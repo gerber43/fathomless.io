3643747ec194567e7594cbbe3808d0d76f3bd463
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-10 17:54:11 -0400

    Update Decor.py
    Updated textureIndex

--- cgi-bin/Decor.py
+++ cgi-bin/Decor.py
@@ -7,7 +7,7 @@ from Biomes import TempBiome
 
 class Door(Decor):
     def __init__(self, pos):
-        super().__init__("Door", 'D', pos, 40, (0.7, 0.7, 0.3, -1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0), False, True, "NO", "")
+        super().__init__("Door", '24', pos, 40, (0.7, 0.7, 0.3, -1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0), False, True, "NO", "")
     def on_interact(self, grid, creature):
         if not self.passable:
             self.passable = True
@@ -20,7 +20,7 @@ class Door(Decor):
 
 class Stairs(Decor):
     def __init__(self, pos):
-        super().__init__("Stairs", "=", pos, 1, (1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0), True, False, "Yes", "Go Down?")
+        super().__init__("Stairs", "23", pos, 1, (1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0), True, False, "Yes", "Go Down?")
     def on_interact(self, current_level, creature):
         #TODO: load new level and change to the new level
         if current_level.depth < 22:
