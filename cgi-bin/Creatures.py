33bd933723abc76cb7ab530e6b960a90b699381b
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-10 17:54:33 -0400

    Update Creatures.py
    Updated textureIndex

--- cgi-bin/Creatures.py
+++ cgi-bin/Creatures.py
@@ -15,4 +15,4 @@ class Goblin(Creature):
             weapon = IronDagger((-1, -1), 1)
         else:
             weapon = WoodenClub((-1, -1), 1)
-        super().__init__("Goblin", "g", pos, (), 10, 0, 1, [], 1, 5, 0, 0.3, 0.5, (weapon, None, None, None, None, None, None, None, None, None), (5, 5, 5, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 10, 0, 2, 0, 2, 0, 5, 0), [], basicDamageResistances, basicStatusResistances, (), 0, ((Gold((-1, -1), 3), 0.7)))
+        super().__init__("Goblin", "22", pos, (), 10, 0, 1, [], 1, 5, 0, 0.3, 0.5, (weapon, None, None, None, None, None, None, None, None, None), (5, 5, 5, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 10, 0, 2, 0, 2, 0, 5, 0), [], basicDamageResistances, basicStatusResistances, (), 0, ((Gold((-1, -1), 3), 0.7)))
