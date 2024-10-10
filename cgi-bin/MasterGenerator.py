439e98621883e289bcdcd0662439fbe5c3bdf4d5
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-10 17:48:27 -0400

    Update MasterGenerator.py
    Updated to allow for tiles with multiple objects

--- cgi-bin/MasterGenerator.py
+++ cgi-bin/MasterGenerator.py
@@ -4,31 +4,39 @@ import json
 import cgi
 import random
 
-HTTP_FIELDS = cgi.FieldStorage()
-
 from GameObject import Terrain
 
 from Terrain import Wall, Pit, Water, Fire, Spikes, EmptySpace  
+from Decor import Stairs, Door
+from Creatures import Goblin
+from GameObject import Creature, CreatureSegment, Gold
+from Items import IronDagger, WoodenClub
 
 def generateMap(algorithm_index,uuid):
     if algorithm_index == 0:
         from GenerateMap import generateMap
     if algorithm_index == 1:
         from PathCarvedMap import generateMap
-    saveMap(uuid, generateMap())
+    saveMap(uuid, generateMap(100,100,1,20))
 
 
 def saveMap(uuid, final_grid):
+    
     dictGrid = []
+    
     for i in range(len(final_grid)):
         tempGrid = []
         for j in range(len(final_grid[0])):
-            if (final_grid[i][j]):
-                tempGrid.append({"terrain": final_grid[i][j].__dict__,"entity": {"textureIndex":8}})
-            else: 
-                tempGrid.append({"terrain": EmptySpace((i, j)).__dict__,"entity": {"textureIndex":8}})
+            internalGrid = {gameObject.__class__.__base__.__name__.lower(): gameObject.__dict__ for gameObject in final_grid[i][j]}
+            if (internalGrid.get('creature') is not None):
+                internalGrid['creature']['equipment'] = ""
+                internalGrid['creature']['drop_table'] = ""
+            else:
+                internalGrid["creature"] = {"textureIndex":8}
+            tempGrid.append(internalGrid)
         dictGrid.append(tempGrid)
-    dictGrid[0][0]['entity'] = {"textureIndex":0}
+    
+    dictGrid[0][0]['creature'] = {"textureIndex":0}
     json_grid = json.dumps(dictGrid)
     with open("../maps/"+uuid+".json", "w") as json_file:
         json_file.write(json_grid)
