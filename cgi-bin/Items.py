f3a40e1849968a91496753215250e4a687247f4c
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-10 17:52:09 -0400

    Update Items.py
    Updated textureIndicies

--- cgi-bin/Items.py
+++ cgi-bin/Items.py
@@ -3,7 +3,7 @@ from SubSystem import lookup_damage_type_id
 
 class IronDagger(Weapon):
     def __init__(self, pos, level):
-        super().__init__("Iron Dagger", ">", pos, level, 5, ("Right Hand", "Left Hand"), "One-Handed Blade", 1, 3, [(lookup_damage_type_id("Piercing"), 3, 1)], [])
+        super().__init__("Iron Dagger", "17", pos, level, 5, ("Right Hand", "Left Hand"), "One-Handed Blade", 1, 3, [(lookup_damage_type_id("Piercing"), 3, 1)], [])
     def on_equip(self, grid, equipped_creature):
         pass
     def on_unequip(self, grid, equipped_creature):
@@ -11,7 +11,7 @@ class IronDagger(Weapon):
 
 class WoodenClub(Weapon):
     def __init__(self, pos, level):
-        super().__init__("Wooden Club", "0", pos, level, 1, ("Right Hand", "Left Hand"), "One-Handed Mace", 1, 1.5, [(lookup_damage_type_id("Blunt"), 5, 0)], [])
+        super().__init__("Wooden Club", "17", pos, level, 1, ("Right Hand", "Left Hand"), "One-Handed Mace", 1, 1.5, [(lookup_damage_type_id("Blunt"), 5, 0)], [])
     def on_equip(self, grid, equipped_creature):
         pass
     def on_unequip(self, grid, equipped_creature):
