75adcae32d5d4f00b330e1f47ed9791b105264f1
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-11 04:31:10 -0400

    Update sessionValidate.php
    allows for difficulty

--- sessionValidate.php
+++ sessionValidate.php
@@ -1,7 +1,10 @@
 <?php
 include("session.php");
-if (isset($_REQUEST['sendDirection'])) {
-    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid.(($_REQUEST['sendDirection'] != "")?"&direction=".$_REQUEST['sendDirection']:""));
+if (isset($_REQUEST['sendDirection']) && isset($uuid)) {
+    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid.(($_REQUEST['sendDirection'] != "")?"&direction=".$_REQUEST['sendDirection']:"").(($_SESSION['difficulty'] != "")?"&difficulty=".$_SESSION['difficulty']:""));
+}
+if (isset($_REQUEST['getMap']) && isset($uuid)) {
+    echo file_get_contents("https://fathomless.io/cgi-bin/map.py?getMap=".$uuid);
 }
 
 
