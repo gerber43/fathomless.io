b51c2cbc321c83da04ec26338b38ffbf6a2358ee
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-11 02:32:50 -0400

    Create mapDestroyer.php
    cron job curls file at beginning of each hour destroying any guest maps that have been inactive for over an hour

---
+++ mapDestroyer.php
@@ -0,0 +1,20 @@
+<?php
+$maps = scandir("maps");
+
+$dbParams = json_decode(file_get_contents("dbPass.pem"),true);
+$conn = new mysqli($dbParams[0],$dbParams[1],$dbParams[2],$dbParams[3]);
+$result = $conn->query("SELECT uuid FROM users");
+$conn->close();
+$uuids = [];
+while ($row = $result->fetch_assoc()) {
+    array_push($uuids,$row['uuid']);
+}
+foreach ($maps as $map) {
+    $currentTime = time();
+    if (str_contains($map,".json")) {
+        if (!in_array(str_replace(".json","",$map),$uuids) && $currentTime - filemtime("maps/".$map) >= 3600) {
+            unlink("maps/".$map);
+        }
+    }
+}
+?>
