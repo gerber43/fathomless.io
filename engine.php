5c593df21b3c0f29c65449d71c906aa9672394eb
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-17 18:38:17 -0400

    Update engine.php
    added sidebar menu inspection

--- engine.php
+++ engine.php
@@ -121,7 +121,7 @@
             font-size:0;
             }
             #settings button {
-                filter: brightness(0) invert(1);
+            filter: brightness(0) invert(1);
             }
             #settings:hover {
             transition:.75s;
@@ -179,25 +179,84 @@
             transform:scale(.9);
             }
             .manhattan {
-                background: purple;
-                opacity:.3;
+            background: purple;
+            opacity:.3;
             }
             .tile[data-manhattan="1"]:hover .light{
-                background: purple;
-                opacity:.3;
-                cursor:pointer;
+            background: purple;
+            opacity:.3;
+            cursor:pointer;
             }
             #cover {
-              position:absolute;
-              top:0;
-              left:0;
-              width:100vw;
-              height:100vh;
-              cursor:wait;
+            position:absolute;
+            top:0;
+            left:0;
+            width:100vw;
+            height:100vh;
+            cursor:wait;
+            }
+            #inspection {
+                position:absolute;
+                top:0;
+                left:0;
+                width:0;
+                height:100vh;
+                background:saddlebrown;
+                overflow:scroll;
+                color:rgb(212,175,55);
+                -ms-overflow-style: none;
+                scrollbar-width: none; 
+                resize:both;
+                cursor: move;
+                
+            }
+            #inspection::-webkit-scrollbar {
+                display: none;
+            }
+            #inspection button {
+                position:absolute;
+                top:0;
+                right:0;
+                width:30px;
+                height:30px;
+                 background: none;
+            color: inherit;
+            border: none;
+            padding: 0;
+            font: inherit;
+            cursor: pointer;
+            outline: inherit;
+                
+            }
+            #inspection div img {
+                width:100px;
+                max-height:100px;
+                position:absolute;
+                top:20px;
+                right:20px;
+                
+            }
+            #inspection div {
+                padding:20px;
+                border-radius:20px;
+                border:solid rgb(212,175,55) 5px;
+                margin:20px;
+                position:relative;
+                
+            }
+            #inspection div p {
+                word-wrap: break-word;
+            }
+            #inspection > p {
+                text-align:center;
+            }
+            .inspecting {
+                transition:.75s;
+                border:#eee dotted 2px;
             }
         </style>
     </head>
-    <body onresize="scaleTextures()">
+    <body>
         <div id = "page">
             <div id = "canvas"></div>
         </div>
@@ -207,21 +266,22 @@
         <div id = "settings">
             <button data-setting = "settings_menu">
                 <img id = "gear" src = "https://icons.veryicon.com/png/o/miscellaneous/xdh-font-graphics-library/gear-setting-1.png">
-                </button>
+            </button>
             <span id = "settings_span">
-                <button data-setting = "toggleAudio">Play SFX</button>
-                <button data-setting = "toggleMusic">Play Music</button>
-                <button data-setting = "toggleAscii">Enable Ascii</button>
-                <button data-setting = "toggleResolution">Toggle Resolution</button>
-                <?=($_SESSION['username'] == "Guest")?'<button data-setting = "saveGuest">Save Progress</button>':'';?>
+            <button data-setting = "toggleAudio">Play SFX</button>
+            <button data-setting = "toggleMusic">Play Music</button>
+            <button data-setting = "toggleAscii">Enable Ascii</button>
+            <button data-setting = "toggleResolution">Toggle Resolution</button>
+            <?=($_SESSION['username'] == "Guest")?'<button data-setting = "saveGuest">Save Progress</button>':'';?>
             </span>
         </div>
+        <div id = "inspection" draggable="true"></div>
         <script>
             const username = '<?=$_SESSION['username']?>';
             const tileObjects = JSON.parse('<?=file_get_contents("https://fathomless.io/json/objects.json")?>');
             const arrowKeys = ["ArrowRight","ArrowDown","ArrowLeft","ArrowUp","KeyD","KeyS","KeyA","KeyW"];
             const defaults = {"terrain":{"textureIndex":3},"light":{"textureIndex":8,"intensity":0},"default":{"textureIndex":8}};
-            var music = playMusic = sfx = isLowResolution = start = disableMovement = playAudio = closeSetting = isSettingsOpen = currentMap = viewDiameter  = inventoryOpened = asciiMode= 0;
+            var inspecting = music = playMusic = sfx = isLowResolution = start = disableMovement = playAudio = closeSetting = isSettingsOpen = currentMap = viewDiameter  = inventoryOpened = asciiMode= 0;
             var objectTypes = ["terrain","item","decor","creature","light"];
             sendDirection();
             window.mobileCheck = function() {let check = false;(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);return check;};
@@ -254,7 +314,47 @@
             }
             function inspectTile(tileId){
                 var coordinates = tileId.split(",");
-                alert("Temporary Tile Inspection Menu: "+JSON.stringify(currentMap[coordinates[0]][coordinates[1]]))
+                var tile = currentMap[coordinates[0]][coordinates[1]]
+                if (inspecting != tileId && tile['terrain']['textureIndex'] != 8) {
+                    document.getElementById(tileId).classList.add('inspecting');
+                if (!inspecting) {
+                    
+                    document.getElementById('inspection').style.width = "50%";
+                } else {
+                    document.getElementById(inspecting).classList.remove('inspecting');
+                }
+                
+                //alert("Temporary Tile Inspection Menu: "+JSON.stringify(currentMap[coordinates[0]][coordinates[1]]));
+                document.getElementById('inspection').innerHTML = "<button onclick = 'document.getElementById(\"inspection\").style.width = 0; document.getElementById(inspecting).classList.remove(\"inspecting\");inspecting = \"\"'>X</button>";
+                
+                Object.keys(tile).forEach((object) => {
+                    if (tile[object]['name']) {
+                    attributes = "";
+                    Object.keys(tile[object]).forEach((attribute) => {
+                        if (attribute == "textureIndex") {
+                            attributes+="<img src = '"+tileObjects[tile[object][attribute]]['icon']+"'>"
+                        } else {
+                            attributes+="<p><span>"+attribute + "</span> : "+tile[object][attribute]+"</p>"
+                        }
+
+                        
+                        
+                    })
+                    document.getElementById('inspection').innerHTML += "<p>"+object+"</p><div>"+attributes+"</div>";
+                    
+                    }
+                    
+                });
+                inspecting = tileId;
+                } else {
+                    if (inspecting) {
+                     document.getElementById(inspecting).classList.remove('inspecting');
+                    }
+                    document.getElementById('inspection').style.width = "0px";
+                                    document.getElementById('inspection').innerHTML = "";
+inspecting = false;
+                }
+                
             }
             function clickTile(tileId) {
                 var coordinates = tileId.split(",");
@@ -266,7 +366,7 @@
             function displayManhattan(distance) {
                 document.getElementById('canvas').querySelectorAll('.manhattan').forEach((object) => {object.classList.remove("manhattan")});
                 for (var i = 1; i <= distance; i++){
-                    document.getElementById('canvas').querySelectorAll(`[data-manhattan*="`+i+`"]`).forEach((object) => {object.querySelector('.light').classList.add("manhattan")});
+                    document.getElementById('canvas').querySelectorAll(`[data-manhattan="`+i+`"]`).forEach((object) => {object.querySelector('.light').classList.add("manhattan")});
                 }
             }
             function updateMap() {
@@ -367,6 +467,9 @@
                 if (e.code === "KeyE")  {
                     toggleInventory();
                 }
+                if (e.code === "Escape")  {
+                    toggleSettings();
+                }
             });
             document.getElementById('settings').addEventListener('mouseenter', (e) => {clearTimeout(closeSetting);});
             document.getElementById('settings').addEventListener('mouseleave', (e) => {
@@ -378,6 +481,10 @@
                 item.addEventListener("click", () => {
                     if (item.dataset.direction && arrowKeys.includes(item.dataset.direction)){
                         directionHandler(item.dataset.direction)
+                    }closeInspecting
+                    if (item.dataset.setting && item.dataset.setting == "closeInspection") {
+                        document.getElementById('inspection').style.width = "0px";
+                        document.getElementById('inspection').innerHTML = "";
                     }
                     if (item.dataset.setting && item.dataset.setting == "settings_menu") {
                         toggleSettings();
@@ -415,6 +522,7 @@
                     }
                 });
             });
+            document.body.onresize = function(){scaleTextures()};
         </script>
     </body>
 </html>
