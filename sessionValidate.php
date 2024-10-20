4698fae57699961953d7455c14c43976ccad32cf
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-20 01:52:19 -0400

    Update sessionValidate.php
    added attack session validation

--- sessionValidate.php
+++ sessionValidate.php
@@ -3,9 +3,10 @@ include("session.php");
 if (isset($_REQUEST['sendDirection']) && isset($uuid)) {
     echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid.(($_REQUEST['sendDirection'] != "")?"&direction=".$_REQUEST['sendDirection']:"").(($_SESSION['difficulty'] != "")?"&difficulty=".$_SESSION['difficulty']:""));
 }
-if (isset($_REQUEST['getMap']) && isset($uuid)) {
-    echo file_get_contents("https://fathomless.io/cgi-bin/map.py?getMap=".$uuid);
+if (isset($_REQUEST['sendAttack']) && isset($uuid)) {
+    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid."&attack=".urlencode($_REQUEST['sendAttack']));
 }
+
 
 
 ?>
