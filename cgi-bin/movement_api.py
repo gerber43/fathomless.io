ad893f657ff67defb165ee1265d999adcf055040
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-20 01:53:21 -0400

    Update movement_api.py
    integrated attack functionality

--- cgi-bin/movement_api.py
+++ cgi-bin/movement_api.py
@@ -4,8 +4,8 @@ import json
 import cgi
 import os
 import pickle
-from user_tracking import a_star
 from map_simplifier import delete_blank_object
+from user_tracking import a_star
 from GameObject import Terrain
 from Creatures import Goblin, Player
 from MasterGenerator import generateMap
@@ -38,10 +38,9 @@ def get_object_by_class(tile,className):
     parsedTile = [gameObject for gameObject in tile if gameObject.__class__.__base__.__name__ == className]
     return None if (len(parsedTile) == 0) else parsedTile[0]
     
-# Function to process creature's movement
-def process_creature_movement(position, direction, game_map):
+# Function to process Creature's movement
+def process_Creature_movement(position, direction, game_map):
     x, y = position
-
     if direction == 0:  # Move right
         new_x, new_y = x + 1, y
     elif direction == 270:  # Move up
@@ -54,32 +53,31 @@ def process_creature_movement(position, direction, game_map):
         return position, "Map Retrieved"
     else:
         return position, "Invalid direction"
-    
     # Validate the new position
     if not is_valid_move(new_x, new_y, game_map):
         return position, "You cannot move here"  # Invalid move
     
-    # Update the creature's position in the map
+    # Update the Creature's position in the map
     
-    message = "creature has moved"
+    message = "Creature has moved"
     get_object_by_class(game_map[x][y],"Creature").move(game_map,[new_x, new_y])
     return (new_x, new_y), message
 
-# Function to update all creature positions
-def update_creature_position(game_map, player_pos):
+# Function to update all Creature positions
+def update_Creature_position(game_map, player_pos):
     
-    moved_creatures = set()  # Track creatures that have already moved
+    moved_Creatures = set()  # Track Creatures that have already moved
     
     for x, row in enumerate(game_map):
         for y, tile in enumerate(row):
-            creature = get_object_by_class(game_map[x][y],"Creature")
+            Creature = get_object_by_class(game_map[x][y],"Creature")
             manhattan = abs(player_pos[0] - x) + abs(player_pos[1] - y)
             check = 3
             
-            if creature != None and manhattan < check and creature.name != "Player":  # if creature exist and not player
+            if Creature != None and manhattan < check and Creature.name != "Player":  # if Creature exist and not player
                 
-                # Check if this creature has already moved in this turn
-                if (x, y) in moved_creatures:
+                # Check if this Creature has already moved in this turn
+                if (x, y) in moved_Creatures:
                     continue
                 
                 speed = 1
@@ -92,10 +90,10 @@ def update_creature_position(game_map, player_pos):
                 for move_num in range(min(speed, len(path) - 1)):
                     next_pos = path[move_num + 1]
                     direction = get_direction_from_step(current_pos, next_pos)  # Get direction for the move
-                    current_pos, message = process_creature_movement(current_pos, direction, game_map)
-                    if message != "creature has moved":
+                    current_pos, message = process_Creature_movement(current_pos, direction, game_map)
+                    if message != "Creature has moved":
                         break
-                moved_creatures.add(current_pos)
+                moved_Creatures.add(current_pos)
 
 # Helper function to get direction between two points
 def get_direction_from_step(current_pos, next_pos):
@@ -126,7 +124,7 @@ def get_map_subset(player_pos, game_map, fov_radius):
     if x_max > 0:
         y_max = len(game_map[0])
 
-    blank_tile = {"terrain": {"textureIndex": 8}}
+    blank_tile = {"Terrain": {"textureIndex": 8}}
     
     map_subset = []
     for i in range(x - fov_radius, x + fov_radius + 1):
@@ -135,14 +133,13 @@ def get_map_subset(player_pos, game_map, fov_radius):
             if i < 0 or i >= x_max or j < 0  or j >= y_max:
                 row_subset.append(blank_tile)
             else:
-
-                internalGrid = {gameObject.__class__.__base__.__name__.lower(): gameObject.__dict__ for gameObject in game_map[i][j]}
-                if (internalGrid.get('creature') is not None):
-                    internalGrid['creature']['equipment'] = [equipment.__dict__ for equipment in internalGrid['creature']['equipment'] if equipment]
-                    internalGrid['creature']['drop_table'] = list(internalGrid['creature']['drop_table'])
-                    for k in range(len(internalGrid['creature']['drop_table'])):
-                        if type(internalGrid['creature']['drop_table'][k]) != type(0.7):
-                            internalGrid['creature']['drop_table'][k] = internalGrid['creature']['drop_table'][k].__dict__
+                internalGrid = {gameObject.__class__.__base__.__name__.capitalize(): gameObject.__dict__ for gameObject in game_map[i][j]}
+                if (internalGrid.get('Creature') is not None):
+                    internalGrid['Creature']['equipment'] = [equipment.__dict__ for equipment in internalGrid['Creature']['equipment'] if equipment]
+                    internalGrid['Creature']['drop_table'] = list(internalGrid['Creature']['drop_table'])
+                    for k in range(len(internalGrid['Creature']['drop_table'])):
+                        if type(internalGrid['Creature']['drop_table'][k]) != type(0.7):
+                            internalGrid['Creature']['drop_table'][k] = internalGrid['Creature']['drop_table'][k].__dict__
                     
                 row_subset.append(internalGrid)
         map_subset.append(row_subset)
@@ -150,8 +147,8 @@ def get_map_subset(player_pos, game_map, fov_radius):
         
     
     return map_subset
-
-
+    
+    
 #Function to get the target creature
 def get_target_creature(game_map, player_pos, tile, fov_radius):
     player_x, player_y = player_pos
@@ -161,31 +158,29 @@ def get_target_creature(game_map, player_pos, tile, fov_radius):
     target_y = fov_y_start + tile[1]
     if target_x < 0 or target_x >= len(game_map) or target_y < 0 or target_y >= len(game_map[0]):
         return None
-    return game_map[target_x][target_y].get("creature")
+    return get_object_by_class(game_map[target_x][target_y],"Creature")
 
 #funtion to process the acttack action
 def process_attack(attacker, target, attack_method = None):
     # calculate the damage based on attack_method, now just set to 1
     if (attack_method == None):
         damage = 1
-
     # reduce target's hp
-    target['hp'] -= damage
+    target.hp -= damage
 
     #check did the creature dead
-    if target['hp'] <= 0:
-        target["textureIndex"] = 8
+    if target.hp <= 0:
+        game_map[target.pos[0]][target.pos[1]].remove(target)
+        return "target killed"
+        #replace with actual death target
     return "target hit"
-
-
-
-
+    
+    
 if (HTTP_FIELDS.getvalue('uuid')):
       uuid = HTTP_FIELDS.getvalue('uuid')
       direction = int(HTTP_FIELDS.getvalue('direction')) if (HTTP_FIELDS.getvalue('direction')) else None
-      target_tile = json.loads(HTTP_FIELDS.getvalue('target_tile')) if HTTP_FIELDS.getvalue('direction') else None
+      attack = HTTP_FIELDS.getvalue('attack').split(",") if HTTP_FIELDS.getvalue('attack') else None
       difficulty = HTTP_FIELDS.getvalue('difficulty') if (HTTP_FIELDS.getvalue('difficulty')) else None
-      
 
     
     # Load the current map
@@ -195,52 +190,53 @@ if (HTTP_FIELDS.getvalue('uuid')):
       else:
           game_map = load_map(file_path)
       
-    # Process player's action
+    # Process player's movement
       player_pos = find_player_position(game_map)
       if (player_pos != None):
           if (direction != None):
-              new_player_pos, message = process_creature_movement(player_pos, direction, game_map)
-          else if (target_tile != None)；
-              filed_of_view = 11
-              fov_radius = filed_of_view // 2
-              target_creature = get_target_creature(game_map, player_pos, target_tile, fov_radius)
+              new_player_pos, message = process_Creature_movement(player_pos, direction, game_map)
+
+          elif (attack != None):
+              attack  = [int(coordinate) for coordinate in attack]
+              field_of_view = 11
+              fov_radius = field_of_view // 2
+              target_creature = get_target_creature(game_map, player_pos, attack, fov_radius)
               if (target_creature):
-                  message = process_attack(game_map[player_pos[0]][player_pos[1]]['creature'], target_creature)
-                  delete_blank_object(game_map)
+                  message = process_attack(get_object_by_class(game_map[player_pos[0]][player_pos[1]],"Creature"), target_creature)
+                  #delete_blank_object(game_map)
               else:
                   message = "no target at this position"
           else:
               message = "invalide action"
       else:
           message = "player not found"
+    #update the Creature's position
+    
       
-    #update the creature's position
-      if (message == "creature has moved" or message == "target hit"):
-          update_creature_position(game_map, player_pos)
-      playerTile = game_map[new_player_pos[0]][new_player_pos[1]]
-      if get_object_by_class(playerTile,"Decor") and get_object_by_class(playerTile,"Decor").name == "Stairs":
-          stair = get_object_by_class(playerTile,"Decor")
-          player = get_object_by_class(game_map[new_player_pos[0]][new_player_pos[1]],"Creature")
+      if (message == "Creature has moved"):
+          update_Creature_position(game_map, player_pos)
+          if get_object_by_class(game_map[new_player_pos[0]][new_player_pos[1]],"Decor") and get_object_by_class(game_map[new_player_pos[0]][new_player_pos[1]],"Decor").name == "Stairs":
+              stair = get_object_by_class(game_map[new_player_pos[0]][new_player_pos[1]],"Decor")
+              player = get_object_by_class(game_map[new_player_pos[0]][new_player_pos[1]],"Creature")
+              
+              depth = str(int(stair.hp.split(",")[0]) + 1)+","+stair.hp.split(",")[1]
+              game_map = generateMap(0, depth)
+    
+              message = "New Map: Level "+str(depth)
+              new_player_pos = find_player_position(game_map)
+              player.pos = [new_player_pos[1],new_player_pos[0]]
           
-          depth = str(int(stair.hp.split(",")[0]) + 1)+","+stair.hp.split(",")[1]
-          game_map = generateMap(0, depth)
-
-          message = "New Map: Level "+str(depth)
-          new_player_pos = find_player_position(game_map)
-          player.pos = [new_player_pos[1],new_player_pos[0]]
-
-          
+              
+          player_pos = new_player_pos
 
     # Save the updated map back to the file
       save_map(file_path, game_map)
       
           
-
     # Caete the subset of the map
       field_of_view = 11
       fov_radius = field_of_view // 2
-      map_subset = get_map_subset(new_player_pos, game_map, fov_radius)
-
+      map_subset = get_map_subset(player_pos, game_map, fov_radius)
 
     #send subset_map and message back to client
       response = {
