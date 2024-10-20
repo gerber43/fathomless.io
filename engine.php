498948264a0322986c25953ef272986996e05bae
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-19 22:05:17 -0400

    Update engine.php
    converted object titles to uppercase

--- engine.php
+++ engine.php
@@ -26,7 +26,7 @@
             }
             #page,#canvas .tile,#inventory,#inventory div {
             display:flex;
-            align-items:center;
+            align-Items:center;
             justify-content:center;
             background-size: contain;
             box-sizing: border-box;
@@ -62,7 +62,7 @@
             }
             #dialogue, #alert {
             display:flex;
-            align-items:center;
+            align-Items:center;
             position:absolute;
             text-align:center;
             color:white;
@@ -92,7 +92,7 @@
             }
             #alert p {
             display:flex;
-            align-items:center;
+            align-Items:center;
             justify-content:center;
             }
             @keyframes fadeSettings {
@@ -111,7 +111,7 @@
             right:0;
             height:100px;
             display:flex;
-            align-items:center;
+            align-Items:center;
             justify-content:end;
             flex-direction:row-reverse;
             width:0px;
@@ -161,7 +161,7 @@
             height:100%;
             width:100%;
             display:flex;
-            align-items:center;
+            align-Items:center;
             justify-content:space-around;
             }
             #settings span * {
@@ -182,7 +182,7 @@
             background: purple;
             opacity:.3;
             }
-            .tile[data-manhattan="1"]:hover .light{
+            .tile[data-manhattan="1"]:hover .Light{
             background: purple;
             opacity:.3;
             cursor:pointer;
@@ -287,9 +287,9 @@
             const username = '<?=$_SESSION['username']?>';
             const tileObjects = JSON.parse('<?=file_get_contents("https://fathomless.io/json/objects.json")?>');
             const arrowKeys = ["ArrowRight","ArrowDown","ArrowLeft","ArrowUp","KeyD","KeyS","KeyA","KeyW"];
-            const defaults = {"terrain":{"textureIndex":3},"light":{"textureIndex":8,"intensity":0},"default":{"textureIndex":8}};
+            const defaults = {"default":{"textureIndex":8,"intensity":0}};
             var inspecting = music = playMusic = sfx = isLowResolution = start = disableMovement = playAudio = closeSetting = isSettingsOpen = currentMap = viewDiameter  = inventoryOpened = asciiMode= 0;
-            var objectTypes = ["terrain","item","decor","creature","light"];
+            var objectTypes = ["Terrain","Item","Decor","Creature","Light"];
             sendDirection();
             window.mobileCheck = function() {let check = false;(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);return check;};
             if (mobileCheck()){toggleResolution()}
@@ -314,15 +314,15 @@
                 document.getElementById('inventory').style.backgroundImage = 'url("'+tileObjects[12]['icon']+'")';
                 document.getElementById('page').style.backgroundImage = 'url("'+tileObjects[13]['icon']+'")';
                 scaleTextures();
-                document.getElementById("canvas").querySelectorAll('.tile').forEach((item) => {
-                    item.addEventListener("click", () => {clickTile(item.id)});
-                    item.addEventListener("contextmenu", function(ev){ev.preventDefault();inspectTile(item.id)});
+                document.getElementById("canvas").querySelectorAll('.tile').forEach((Item) => {
+                    Item.addEventListener("click", () => {clickTile(Item.id)});
+                    Item.addEventListener("contextmenu", function(ev){ev.preventDefault();inspectTile(Item.id)});
                 });
             }
             function inspectTile(tileId){
                 var coordinates = tileId.split(",");
                 var tile = currentMap[coordinates[0]][coordinates[1]]
-                if (inspecting != tileId && tile['terrain']['textureIndex'] != 8) {
+                if (inspecting != tileId && tile['Terrain']['textureIndex'] != 8) {
                     document.getElementById(tileId).classList.add('inspecting');
                 if (!inspecting) {
                     
@@ -401,13 +401,13 @@ inspecting = false;
             function displayManhattan(distance) {
                 document.getElementById('canvas').querySelectorAll('.manhattan').forEach((object) => {object.classList.remove("manhattan")});
                 for (var i = 1; i <= distance; i++){
-                    document.getElementById('canvas').querySelectorAll(`[data-manhattan="`+i+`"]`).forEach((object) => {object.querySelector('.light').classList.add("manhattan")});
+                    document.getElementById('canvas').querySelectorAll(`[data-manhattan="`+i+`"]`).forEach((object) => {object.querySelector('.Light').classList.add("manhattan")});
                 }
             }
             function updateMap() {
                 for (var i = 0; i < viewDiameter; i++) {
                     for (var j = 0; j < viewDiameter; j++) {
-                        objectTypes.forEach((object) => {applyTexture(object,j+","+i,(currentMap[j][i][object])?currentMap[j][i][object]:(defaults[object]?defaults[object]:defaults["default"]));});
+                        objectTypes.forEach((object) => {applyTexture(object,j+","+i,(currentMap[j][i] && object in currentMap[j][i])?currentMap[j][i][object]:defaults["default"]);});
                     }
                 }
             }
@@ -428,7 +428,7 @@ inspecting = false;
                     selectedElement.style.backgroundImage = 'url("'+tileObjects[object['textureIndex']][asciiMode?"ascii":"icon"]+'")';
                 } else {
                     selectedElement.style.background = tileObjects[object['textureIndex']]["color"];
-                    if (type == "creature") {
+                    if (type == "Creature") {
                         selectedElement.style.borderRadius = "50%";
                     }
                 }
@@ -471,11 +471,16 @@ inspecting = false;
                         }
                         currentMap = response["map_subset"];
                         updateMap();
+                        var player = document.getElementById(Math.floor(viewDiameter/2)+","+Math.floor(viewDiameter/2)).querySelector(".Creature");
+                        player.style.transition = "";
+                        player.style.transform = "";
+
                         createMessage("dialogue",response["message"],1);
+                        
                         disableMovement = false;
                         console.log(`Time taken: `+(performance.now() - start)+` milliseconds`);
                         displayManhattan(0);
-                        if (response["message"] == "creature has moved"){playSound(0);}
+                        if (response["message"] == "Creature has moved"){playSound(0);}
                         if (response["message"].includes("New Map")){playSound(4);}
                         if (response["message"].includes("Current HP")){playSound(8);}
                         if (document.getElementById("cover")){
@@ -489,6 +494,11 @@ inspecting = false;
             }
             function directionHandler(keycode) {
                 if (!disableMovement) {
+                    var player = document.getElementById(Math.floor(viewDiameter/2)+","+Math.floor(viewDiameter/2)).querySelector(".Creature");
+                    var offset = player.getBoundingClientRect().width;
+                    player.style.transition = ".2s";
+                    player.style.zIndex = 100000
+                    player.style.transform = "translate("+(offset*Math.cos((arrowKeys.indexOf(keycode)%4)*Math.PI/2))+"px, "+(offset*Math.sin((arrowKeys.indexOf(keycode)%4)*Math.PI/2))+"px)";
                     document.body.appendChild(Object.assign(document.createElement('div'),{id:("cover")}));
                     start = performance.now();
                     sendDirection((arrowKeys.indexOf(keycode)%4)*90);
@@ -518,23 +528,23 @@ inspecting = false;
                     closeSetting = setTimeout(toggleSettings, 3000);
                 }
             });
-            document.querySelectorAll('button').forEach((item) => {
-                item.addEventListener("click", () => {
-                    if (item.dataset.direction && arrowKeys.includes(item.dataset.direction)){
-                        directionHandler(item.dataset.direction)
+            document.querySelectorAll('button').forEach((Item) => {
+                Item.addEventListener("click", () => {
+                    if (Item.dataset.direction && arrowKeys.includes(Item.dataset.direction)){
+                        directionHandler(Item.dataset.direction)
                     }
-                    if (item.dataset.setting && item.dataset.setting == "settings_menu") {
+                    if (Item.dataset.setting && Item.dataset.setting == "settings_menu") {
                         toggleSettings();
                     }
-                    if (item.dataset.setting && item.dataset.setting == "toggleAudio") {
-                        item.innerHTML = (!playAudio)?"Mute SFX":"Play SFX"
+                    if (Item.dataset.setting && Item.dataset.setting == "toggleAudio") {
+                        Item.innerHTML = (!playAudio)?"Mute SFX":"Play SFX"
                         playAudio = !playAudio;
                          if (sfx == 0 && playAudio) {
                             sfx = [new Audio('https://fathomless.io/assets/audio/walking.mp3'),new Audio('https://fathomless.io/assets/audio/coin.mp3'),new Audio('https://fathomless.io/assets/audio/crash.mp3'),new Audio('https://fathomless.io/assets/audio/walk2.mp3'), new Audio('https://fathomless.io/assets/audio/woosh.mp3'), new Audio('https://fathomless.io/assets/audio/dead.mp3'), new Audio('https://fathomless.io/assets/audio/attack.mp3'), new Audio('https://fathomless.io/assets/audio/shoot.mp3'), new Audio('https://fathomless.io/assets/audio/hit.mp3'), new Audio('https://fathomless.io/assets/audio/kill.mp3')];
                         }
                     }
-                    if (item.dataset.setting && item.dataset.setting == "toggleMusic") {
-                        item.innerHTML = (!playMusic)?"Mute Music":"Play Music"
+                    if (Item.dataset.setting && Item.dataset.setting == "toggleMusic") {
+                        Item.innerHTML = (!playMusic)?"Mute Music":"Play Music"
                         playMusic = !playMusic;
                          if (music == 0 && playMusic) {
                             music = [new Audio('https://fathomless.io/assets/audio/track1.mp3')];
@@ -547,14 +557,14 @@ inspecting = false;
                             music[0].currentTime = 0;
                         }
                     }
-                    if (item.dataset.setting && item.dataset.setting == "toggleAscii") {
-                        item.innerHTML = (!asciiMode)?"Enable Graphics":"Enable Ascii"
+                    if (Item.dataset.setting && Item.dataset.setting == "toggleAscii") {
+                        Item.innerHTML = (!asciiMode)?"Enable Graphics":"Enable Ascii"
                         toggleAscii();
                     }
-                    if (item.dataset.setting && item.dataset.setting == "toggleResolution") {
+                    if (Item.dataset.setting && Item.dataset.setting == "toggleResolution") {
                         toggleResolution();
                     }
-                    if (item.dataset.setting && item.dataset.setting == "saveGuest") {
+                    if (Item.dataset.setting && Item.dataset.setting == "saveGuest") {
                         window.location.href = "https://fathomless.io/saveGuest/";
                     }
                 });
