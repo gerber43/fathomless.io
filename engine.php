46dbcc48c4a50d8eccc652c33c746b7b5f02cb38
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-25 01:56:00 -0400

    Update engine.php
    removed move by degree

--- engine.php
+++ engine.php
@@ -27,7 +27,7 @@
             }
             #page,#canvas .tile,#inventory,#inventory div, #keyBind {
             display:flex;
-            align-Items:center;
+            align-items:center;
             justify-content:center;
             background-size: contain;
             box-sizing: border-box;
@@ -63,7 +63,7 @@
             }
             #dialogue, #alert {
             display:flex;
-            align-Items:center;
+            align-items:center;
             position:absolute;
             text-align:center;
             color:white;
@@ -93,7 +93,7 @@
             }
             #alert p {
             display:flex;
-            align-Items:center;
+            align-items:center;
             justify-content:center;
             }
             @keyframes fadeSettings {
@@ -112,7 +112,7 @@
             right:0;
             height:100px;
             display:flex;
-            align-Items:center;
+            align-items:center;
             justify-content:end;
             flex-direction:row-reverse;
             width:0px;
@@ -166,7 +166,7 @@
             height:100%;
             width:100%;
             display:flex;
-            align-Items:center;
+            align-items:center;
             justify-content:space-around;
             }
             #settings span * {
@@ -202,15 +202,18 @@
                 top:0;
                 left:0;
                 width:0;
-                height:100vh;
+                height:100svh;
                 background:saddlebrown;
                 overflow:scroll;
                 color:rgb(212,175,55);
                 -ms-overflow-style: none;
                 scrollbar-width: none; 
                 resize:both;
-                cursor: move;
-                
+                display:flex;
+                align-items:center;
+                justify-content:start;
+                flex-direction:column;
+
             }
             #inspection::-webkit-scrollbar {
                 display: none;
@@ -238,13 +241,15 @@
 
             }
             #inspection > div > div {
-                width:80%;
+                width:90%;
             }
             #inspection div {
-                padding:20px;
+                padding:10px;
                 border-radius:20px;
                 border:solid rgb(212,175,55) 5px;
-                margin:20px;
+                margin:10px;
+                width:90%;
+                
 
             }
             #inspection div p {
@@ -311,7 +316,7 @@
             var keyBinds = (!localStorage.getItem("keyBinds"))?{"Inventory":"KeyE","Settings":"Escape","Attack":"Space","Movement":["ArrowRight","ArrowDown","ArrowLeft","ArrowUp","KeyD","KeyS","KeyA","KeyW"]}:(JSON.parse(localStorage.getItem("keyBinds")));
             sendRequest("?sendDirection");
             window.mobileCheck = function() {let check = false;(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);return check;};
-            if (mobileCheck()){toggleResolution()}
+            //if (mobileCheck()){toggleResolution()}
             function toggleSettings() {
                 document.getElementById('settings').style.width=(isSettingsOpen?"0px":"100vw");
                 document.getElementById('settings').style.fontSize=(isSettingsOpen?"0px":"10px");
@@ -328,6 +333,23 @@
                         tile.dataset.manhattan = Math.abs(j - Math.floor(viewDiameter/2)) + Math.abs(i - Math.floor(viewDiameter/2));
                         objectTypes.forEach((object) => {tile.appendChild(Object.assign(document.createElement('div'),{classList:object}))});
                         document.getElementById('canvas').appendChild(tile);
+                        tile.addEventListener('touchstart', (event) => {
+                            
+                
+  this.touchStart = event.touches[0];
+
+  // Clear any existing timeout
+  clearTimeout(this.longPressTimeout);
+
+  // Set a timeout for long press detection
+  this.longPressTimeout = setTimeout(() => {
+    // Trigger the right-click behavior here
+    alert(j+","+i)
+    inspectTile(j+","+i)
+  }, 500); // Adjust the timeout duration as needed
+});
+                    
+                        
                     }
                 }
                 document.getElementById('dialogue').style.backgroundImage = 'url("'+tileObjects[12]['icon']+'")';
@@ -338,6 +360,7 @@
                     Item.addEventListener("click", () => {clickTile(Item.id)});
                     Item.addEventListener("contextmenu", function(ev){ev.preventDefault();inspectTile(Item.id)});
                 });
+                
             }
             function toggleInspect(tileId){
                 var tile = currentMap[tileId.split(",")[0]][tileId.split(",")[1]]
@@ -362,7 +385,7 @@
                     if (tile[object]['name']) {
                         attributes = "";
                         Object.keys(tile[object]).forEach((attribute) => {
-                            specialFormats = ["equipment","drop_table"];
+                            specialFormats = ["equipment","drop_table","inventory"];
                             if (!specialFormats.includes(attribute)) {
                                 if (attribute == "textureIndex") {
                                     attributes+="<img src = '"+tileObjects[tile[object][attribute]]['icon']+"'>"
@@ -394,17 +417,12 @@
                 });
             }
             function clickTile(tileId) {
+   
                 var coordinates = tileId.split(",");
                 if (inspecting) {
                     toggleInspect(inspecting);
-                }
-                if (document.getElementById(tileId).dataset.manhattan == 1 && !attacking) {
-                    var index = Math.atan2(coordinates[1] - Math.floor(viewDiameter/2),coordinates[0] - Math.floor(viewDiameter/2))*2/Math.PI
-                    directionHandler(keyBinds["Movement"][(index != -1)?index:3])
-                }
-                if (attacking) {
-                    sendRequest("?sendAttack="+encodeURIComponent(tileId));
-                    toggleAttack();
+                } else {
+                    directionHandler(coordinates)
                 }
             }
             function displayManhattan(distance) {
@@ -548,23 +566,25 @@
                 xmlhttp.onreadystatechange = function () {
                     if (this.readyState == 4 && this.status == 200) {
                         receiveMap(this.responseText)
+                        inspectTile("5,5")
+                        
                     }
                 }
                 xmlhttp.open("GET", "https://fathomless.io/sessionValidate/"+uri, true);
                 xmlhttp.send();
             }
             function moveObject(gameObject,x,y) {
+                var offset = document.getElementById("0,0").getBoundingClientRect().width;
                 gameObject.style.transition = (x != 0 || y != 0)?".2s":"0s";
                 gameObject.style.transitionTimingFunction = (x != 0 || y != 0)?"ease-in-out":"";
-                gameObject.style.transform = (x != 0 || y != 0)?"translate("+x+"px, "+y+"px)":"";
+                gameObject.style.transform = (x != 0 || y != 0)?"translate("+(offset*x)+"px, "+(offset*y)+"px)":"";
             }
-            function directionHandler(keycode) {
+            function directionHandler(tileCoordinates) {
                 if (!disableMovement) {
-                    sendRequest("?sendDirection="+((keyBinds["Movement"].indexOf(keycode)%4)*90));
+                    sendRequest("?sendAttack="+encodeURIComponent(tileCoordinates));
                     disableMovement = true;
                     var player = document.getElementById(Math.floor(viewDiameter/2)+","+Math.floor(viewDiameter/2)).querySelector(".Creature");
-                    var offset = player.getBoundingClientRect().width;
-                    var direction  = [offset*Math.cos((keyBinds["Movement"].indexOf(keycode)%4)*90*Math.PI/180),offset*Math.sin((keyBinds["Movement"].indexOf(keycode)%4)*90*Math.PI/180)];
+                    var direction  = [(tileCoordinates[0]-Math.floor(viewDiameter/2)),(tileCoordinates[1]-Math.floor(viewDiameter/2))];
                     moveObject(player,direction[0],direction[1]);
                     player.style.zIndex = 1
                     moveObject(document.getElementById('canvas'),-direction[0],-direction[1]);
@@ -585,7 +605,9 @@
                     changeKeyBind(e.code);
                 } else {
                 if (keyBinds["Movement"].indexOf(e.code) > -1) {
-                    directionHandler(e.code)
+                    var deg = (keyBinds["Movement"].indexOf(e.code)%4)*Math.PI/2;
+                    target = [Math.floor(viewDiameter/2)+Math.round(Math.cos(deg)),Math.floor(viewDiameter/2)+Math.round(Math.sin(deg))]
+                    directionHandler(target);
                 }
                 if (e.code === keyBinds['Inventory'])  {
                     toggleInventory();
@@ -617,9 +639,6 @@
             });
             document.querySelectorAll('button').forEach((Item) => {
                 Item.addEventListener("click", () => {
-                    if (Item.dataset.direction && keyBinds["Movement"].includes(Item.dataset.direction)){
-                        directionHandler(Item.dataset.direction)
-                    }
                     if (Item.dataset.setting && Item.dataset.setting == "settings_menu") {
                         toggleSettings();
                     }
@@ -660,6 +679,7 @@
                 });
             });
             document.body.onresize = function(){scaleTextures()};
+
         </script>
     </body>
 </html>
