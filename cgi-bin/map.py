500e076d7cad05cca661fe6143d223aa02c42a27
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-10-25 02:07:00 -0400

    Delete cgi-bin/map.py
    no longer used

--- cgi-bin/map.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import sys
-import cgi #https://docs.python.org/3/library/cgi.html
-print ('Content-type:text/html\n') 
-HTTP_FIELDS = cgi.FieldStorage() 
-if (HTTP_FIELDS.getvalue("getMap")):
-  f = open("../json/map.json", "r").read()
-  sys.stdout.write(f)
