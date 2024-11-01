5c701b55ccfa0da06aa1c3843b4316baa6c94190
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-11-01 03:28:54 -0400

    Update sessionValidate.php
    updated sessionvalidation to account for inventory update paremeters

--- sessionValidate.php
+++ sessionValidate.php
@@ -1,12 +1,16 @@
 <?php
 include("session.php");
-if (isset($_REQUEST['sendDirection']) && isset($uuid)) {
-    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid.(($_REQUEST['sendDirection'] != "")?"&direction=".$_REQUEST['sendDirection']:"").(($_SESSION['difficulty'] != "")?"&difficulty=".$_SESSION['difficulty']:""));
-}
 if (isset($_REQUEST['sendAttack']) && isset($uuid)) {
-    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid."&attack=".urlencode($_REQUEST['sendAttack']));
+    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid."&attack=".urlencode($_REQUEST['sendAttack']).(isset($_REQUEST['selected'])?"&selected=".urlencode($_REQUEST['selected']):""));
+
+    
+    
+} else if(!isset($_REQUEST['sendAttack']) && isset($uuid)) {
+    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid);
 }
 
-
+if (!isset($uuid)) {
+    echo "Timed Out";
+}
 
 ?>
