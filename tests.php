56b362c518eb301ff5c20f05e827784c51f8ff81
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-11-01 03:29:23 -0400

    Update tests.php
    added sql injection test case

--- tests.php
+++ tests.php
@@ -1,5 +1,9 @@
-<p>The First Row Is The Spawn List For Each Level</p>
+
 <style>
+html, body {
+    margin:0;
+    padding:0;
+}
     table, tr,td,th{
         border:solid black 2px;
     }
@@ -8,10 +12,53 @@
         background:#eee;
     }
 </style>
+
 <?php 
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
+error_reporting(E_ERROR | E_PARSE);
+
+
+
+$malStrings = ["username","<username>' OR 1=1--
+'OR '' = '	Allows authentication without a valid username.
+<username>'--
+' union select 1, '<user-fieldname>', '<pass-fieldname>' 1--
+'OR 1=1--",'1d3d2d231d2dd4570017" OR "570017"="570017',"1d3d2d231d2dd4570017' OR '570017'='570017","SELECT * from users where user = 'Test'",];
+
+echo '<p>The first row demonstrates a successful, non malicious string</p>';
+echo '<table><tr><th>Malicious String</th><th>HTTP Response Header</th></tr>';
+
+
+foreach($malStrings as $string) {
+    $url = 'https://fathomless.io';
+$data = ['create_account' => '', 'username' => $string, 'password' => "pass", "difficulty" => "Easy"];
+
+$options = [
+    'http' => [
+        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method' => 'POST',
+        'content' => http_build_query($data),
+    ],
+];
+
+$context = stream_context_create($options);
+file_get_contents($url, false, $context);
+
+echo "<tr><td>".$string."</td><td>".$http_response_header[0]."</td>";
+
+}
+echo '</table>';
+
+
+echo '<p>In addition to these tests an SQL Injection attack test was performed by <a target = "_blank" href = "https://pentest-tools.com">https://pentest-tools.com</a>. The results of which are displayed here.</p>';
+echo '<p>The test was performed with 27 Unique Injection Points,
+across 13 URLS, with 1089 HTTP requests</p><embed style = "width:100vw;height:50vh" src = "https://fathomless.io/inject.pdf">';
+
+
+
+echo '<p>The First Row Is The Spawn List For Each Level</p>';
 function generateMap($level = 0) {
 if (file_exists("logs/Test.txt")) {
     unlink("logs/Test.txt");
