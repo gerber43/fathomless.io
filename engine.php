ed84076429ab953e312577cbb8732d8447261e75
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-11-10 02:55:03 -0500

    Update engine.php
    added ability to level up

--- engine.php
+++ engine.php
@@ -388,7 +388,7 @@ if (file_exists("maps/Test.pkl")) {
   outline: none; /* Remove outline */
   color:rgb(212,175,55);
         }
-        .modal div {
+        .modal > div {
             margin: 0;
             position: absolute;
             top: 50%;
@@ -403,9 +403,10 @@ if (file_exists("maps/Test.pkl")) {
             background:saddlebrown;
             gap:20px;
             border:rgb(212,175,55) 2px solid;
+            height:fit-content;
 
         }
-        .modal div .log {
+        .modal > div .log {
             padding:20px;
             height:200px;
             overflow:scroll;
@@ -434,6 +435,33 @@ if (file_exists("maps/Test.pkl")) {
             background:url(https://img.freepik.com/premium-vector/seamless-pattern-old-wood-wall-background_117579-47.jpg);
             
         }
+        
+        #levelUpMenu div {
+            width:100%;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            background:url(https://img.freepik.com/premium-vector/seamless-pattern-old-wood-wall-background_117579-47.jpg);
+            gap:0;
+
+        }
+        #levelUpMenu {
+            padding:0;
+            margin:0;
+        }
+        #levelUpMenu div button {
+           
+            width:50px;
+            height:100%;
+            padding:0;
+            margin:0;
+            outline:0;
+            border:0;
+            font-size:30px;
+                        background:none;
+
+        }
+        
         #inventory span {
             display:flex;
             align-items:center;
@@ -544,11 +572,15 @@ if (file_exists("maps/Test.pkl")) {
             sendRequest();
             window.mobileCheck = function() {let check = false;(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);return check;};
             //if (mobileCheck()){toggleResolution()}
-            function createModal(content) {
+            function createModal(content, id = "") {
+                disableMovement = true;
+                if (document.getElementById(`modal`)) {
+                document.getElementById(`modal`).remove();
+                }
                 var modal = Object.assign(document.createElement('div'),{classList:"modal",id:"modal"});
                       var innerDiv = "";
                        innerDiv +=  content;
-                       modal.innerHTML = "<div>"+innerDiv+"</div>";
+                       modal.innerHTML = "<div id = '"+id+"'>"+innerDiv+"</div>";
                         document.body.appendChild(modal);
             }
             
@@ -646,14 +678,12 @@ if (file_exists("maps/Test.pkl")) {
                     var range = (currentMap[viewRadius][viewRadius]["Creature"]["equipment"][0]['range'])
                     if ((target["Bottom"]["textureIndex"] != 8) && ((document.getElementById(coordinates[0]+","+coordinates[1]).dataset.manhattan <= range && target["Creature"] !== undefined) || document.getElementById(coordinates[0]+","+coordinates[1]).dataset.manhattan <= 1)) {
                     if (target["Terrain"] && target["Terrain"]["warning"] != "" && target["Creature"] === undefined) {
-                        disableMovement = true;
-                         createModal("<p>"+target["Terrain"]["warning"]+"</p><button onclick = 'this.parentElement.parentElement.remove();disableMovement=false;sendRequest(`?sendAttack=`+encodeURIComponent(confirmationCoordinates));'>Yes</button><button onclick = 'this.parentElement.parentElement.remove();disableMovement=false;'>No</button>");
+                         createModal("<p>"+target["Terrain"]["warning"]+"</p><button onclick = 'this.parentElement.parentElement.remove();sendRequest(`?sendAttack=`+encodeURIComponent(confirmationCoordinates));'>Yes</button><button onclick = 'this.parentElement.parentElement.remove();disableMovement=false;'>No</button>");
 confirmationCoordinates = coordinates;
                         return false
                     }
                     if (target["Decor"] && target["Decor"]["warn"] == "Yes"  && target["Creature"] === undefined) {
-                        disableMovement = true;
-                         createModal("<p>"+target["Decor"]["warning"]+"</p><button onclick = 'this.parentElement.parentElement.remove();disableMovement=false;sendRequest(`?sendAttack=`+encodeURIComponent(confirmationCoordinates));'>Yes</button><button onclick = 'this.parentElement.parentElement.remove();disableMovement=false;'>No</button>");
+                         createModal("<p>"+target["Decor"]["warning"]+"</p><button onclick = 'this.parentElement.parentElement.remove();sendRequest(`?sendAttack=`+encodeURIComponent(confirmationCoordinates));'>Yes</button><button onclick = 'this.parentElement.parentElement.remove();disableMovement=false;'>No</button>");
 confirmationCoordinates = coordinates;
 
                         return false
@@ -712,7 +742,7 @@ confirmationCoordinates = coordinates;
                                                 buffer += "<p>"+objectKeys[j]+" : "+attributeArray[i][objectKeys[j]]+"</p>";
                                             }
                                         }
-                                        if (attribute == "inventory" || attribute == "abilities")
+                                        if ((attribute == "inventory" || attribute == "abilities") && attributeArray[i]['name'] != "Gold")
                                             buffer += "<button class = 'use' onclick = 'selectedItem = `"+attribute+":"+i+"`;toggleInspect(`"+tileId+"`);createMessage(`dialogue`,`Click On A Target`,2);'>Use</button>";
 
                                     } else {
@@ -894,9 +924,27 @@ confirmationCoordinates = coordinates;
                             music.play();
                 }
             }
+                            var statusPoints = {"cunning":0,"fitness":0,"magic":0,"total":0};
+
+            function updateLevelUp(status, direction) {
+                    var pointsUsed = statusPoints['cunning'] + statusPoints["fitness"] + statusPoints["magic"];
+
+                    if (status != "submit") {
+                        statusPoints[status] += (statusPoints[status] + direction >= 0 && pointsUsed + direction <= statusPoints['total'])?direction:0;
+                        document.getElementById(status).innerHTML = statusPoints[status];
+                        document.getElementById('remaining_points').innerHTML = statusPoints['total'] - (statusPoints['cunning'] + statusPoints["fitness"] + statusPoints["magic"]);
+                        
+                    } else {
+                        sendRequest("?levelUp&cunning="+encodeURIComponent(statusPoints['cunning'])+"&fitness="+encodeURIComponent(statusPoints['fitness'])+"&magic="+encodeURIComponent(statusPoints['magic']));
+                    }
+                }
             function receiveMap(response) {
                 console.log(`Time taken: `+(performance.now() - start)+` milliseconds`);
-                response = JSON.parse(response);
+                if (response != "Timed Out") {
+                    response = JSON.parse(response);
+                } else {
+                    createModal("<p>Timed Out</p><button onclick = 'location.href = `https://fathomless.io`'>Home</button>")
+                }
                 
                 if (response['map_subset'].length != viewDiameter) {
                     viewDiameter = response['map_subset'].length;
@@ -907,6 +955,7 @@ confirmationCoordinates = coordinates;
                 var moved = [];
                 var playerDamage = 0;
                 var gameOver = 0
+
                 response["turn_log"].forEach((update) => {
                     if (update['type'] == "attack") {
                         applyEffects(document.getElementById((update['after'][0]+playerDirection[0])+","+(update['after'][1]+playerDirection[1])).querySelector(".Top"),"damaged",.2)
@@ -914,7 +963,24 @@ confirmationCoordinates = coordinates;
                             playerDamage -= update['amount'];
                         }
                     }
+                    if (update['level_up_menu']) {statusPoints
+                        statusPoints['total'] = update['level_up_menu']['points'];
+                        var levelUpMenu = "<p>New Level: "+update['level_up_menu']['level']+"</p>";
+                        levelUpMenu += "<p>Remaining Points <span id = 'remaining_points'>"+(update['level_up_menu']['points']-statusPoints['cunning']-statusPoints['fitness']-statusPoints['magic'])+"</span></p>";
+                        levelUpMenu += "<p>Current Cunning: "+currentMap[viewRadius][viewRadius]['Creature']['cunning']+"</p>";
+                        levelUpMenu += "<div><button onclick = 'updateLevelUp(`cunning`, -1);'>-</button><p>Cunning <span id = 'cunning'>"+statusPoints['cunning']+"</span></p><button onclick = 'updateLevelUp(`cunning`, 1);'>+</button></div>";
+                        levelUpMenu += "<p>Current Fitness: "+currentMap[viewRadius][viewRadius]['Creature']['fitness']+"</p>";
+                        levelUpMenu += "<div><button onclick = 'updateLevelUp(`fitness`, -1);'>-</button><p>Fitness <span id = 'fitness'>"+statusPoints['fitness']+"</span></p><button onclick = 'updateLevelUp(`fitness`, 1);'>+</button></div>";
+                        levelUpMenu += "<p>Current Magic: "+currentMap[viewRadius][viewRadius]['Creature']['magic']+"</p>";
+                        levelUpMenu += "<div><button onclick = 'updateLevelUp(`magic`, -1);'>-</button><p>Magic <span id = 'magic'>"+statusPoints['magic']+"</span></p><button onclick = 'updateLevelUp(`magic`, 1);'>+</button></div>";
+                        levelUpMenu += "<button onclick = 'updateLevelUp(`submit`, 0);'>Submit</button>";
+                        createModal(levelUpMenu,"levelUpMenu");
+                    }
                     if (update['level_up']) {
+                        if (document.getElementById('modal')) {
+                            document.getElementById('modal').remove();
+                        }
+                        statusPoints = {"cunning":0,"fitness":0,"magic":0,"total":0};
                         playSound(9)
                     }
                     if (update['level']) {
@@ -941,9 +1007,10 @@ confirmationCoordinates = coordinates;
                 }
                 setTimeout(function(){ 
                     updateMap();
-                    disableMovement = false;
+                    if (!document.getElementById('levelUpMenu')) {
+                        disableMovement = false;
+                    }
                     if (gameOver) {
-                        disableMovement = true;
                         var endMessage = (currentLevel == 23)?"Congratulations, You Win!":"Game Over!";
                         if (currentLevel != 23) {
                             playSound(5)
