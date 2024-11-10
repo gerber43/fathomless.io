12dde0ed18143ee3877882ef99988293fed01273
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-11-10 02:53:29 -0500

    Update sessionValidate.php
    added level up session validation

--- sessionValidate.php
+++ sessionValidate.php
@@ -5,10 +5,13 @@ if (isset($_REQUEST['sendAttack']) && isset($uuid)) {
 
     
     
-} else if(!isset($_REQUEST['sendAttack']) && isset($uuid)) {
-    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid);
-}
+}  else if (isset($_REQUEST['levelUp']) && isset($uuid)) {
+    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid.(isset($_REQUEST['cunning'])?"&levelUp=levelUp&cunning=".urlencode($_REQUEST['cunning']):"").(isset($_REQUEST['magic'])?"&magic=".urlencode($_REQUEST['magic']):"").(isset($_REQUEST['fitness'])?"&fitness=".urlencode($_REQUEST['fitness']):""));
 
+}
+else if(!isset($_REQUEST['sendAttack']) && isset($uuid)) {
+    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid);
+} 
 if (!isset($uuid)) {
     echo "Timed Out";
 }
