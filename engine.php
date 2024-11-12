208e9d970038a627fef0ad4811ac31bcb7cf5db6
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-11-12 02:23:26 -0500

    Update engine.php
    action selector

--- engine.php
+++ engine.php
@@ -480,27 +480,72 @@ if (file_exists("maps/Test.pkl")) {
                         background:none;
 
         }
-        
+        #inventory span * {
+            padding:0;
+            margin:0;
+        }
         #inventory span {
+            padding:20px;
+        }
+        #inventory > span {
             display:flex;
             align-items:center;
             justify-content:center;
             flex-direction:column;
-            width:200px;
-            height:200px;
+            width:100%;
+            height:fit-content;
             font-size:20px;
             text-align:center;
              border:rgb(212,175,55) 2px solid;
             background:saddlebrown;
             color:rgb(212,175,55);
             gap:0px;
+            
+            
+        }#inventory > span > span {
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            flex-direction:row;
+            flex-wrap:wrap;
+            width:100%;
+            height:fit-content;
+            
+
         }
-        #inventory span * {
-            padding:0;
-            margin:0;
+        
+        #inventory > span > span > span {
+            display:flex;
+            align-items:center;
+            justify-content:space-between;
+            flex-direction:column;
+            width:fit-content;
+            height:fit-content;
+             border:rgb(212,175,55) 2px solid;
+             font-size:15px;
+             width:200px;
+             height:200px;
+             gap:20px
         }
-        #inventory div img {
-            height:100px;
+        #inventory > span > span > span p {
+            height:30px;
+        }
+         #inventory > span > span > span button {
+             transition:.75s;
+            background: none;
+            color: inherit;
+            border: none;
+            padding: 0;
+            font: inherit;
+            cursor: pointer;
+            outline: inherit;
+            color:red;
+            
+
+         }
+        
+        #inventory img {
+            height:50px;
             }
             #inventory {
                 flex-direction:row;
@@ -514,9 +559,21 @@ if (file_exists("maps/Test.pkl")) {
             .tile .Top {
                 z-index:3;
             }
-            #inventory, #settings, #alert, #dialogue, #keyBind, #modal, #inspection {
+            #inventory, #settings, #alert, #dialogue, #keyBind, #modal, #inspection, #action {
                 z-index:4;
             }
+            #action {
+                display:flex;
+                align-items:center;
+                justify-content:center;
+                width:100vw;
+                gap:20px;
+                position:absolute;
+                top:0;
+                left:0;
+                background:#eee;
+            }
+           
             
         </style>
     </head>
@@ -546,12 +603,13 @@ if (file_exists("maps/Test.pkl")) {
                 <?=($_SESSION['username'] == "Guest")?'<button data-setting = "saveGuest">Save Progress</button>':'<button data-setting = "logout">Logout</button>';?>
             </span>
         </div>
-        <div id = "inspection" draggable="true"></div>
+        <div id = "inspection"></div>
+        <div id = "action"></div>
         <script>
             const username = '<?=$_SESSION['username']?>';
             const tileObjects = JSON.parse('<?=file_get_contents("https://fathomless.io/json/objects.json")?>');
             const defaults = {"default":{"textureIndex":8,"intensity":0},"Bottom":{"textureIndex":1}};
-            var songPosition = sfxVolume = musicVolume = currentLevel = playerDirection = viewRadius = keyBindOpened = inspecting = music = playMusic = sfx = isLowResolution = start = disableMovement = playAudio = closeSetting = isSettingsOpen = currentMap = viewDiameter  = inventoryOpened = asciiMode= 0;
+            var inspectingPlayer = activeTile = isAction = songPosition = sfxVolume = musicVolume = currentLevel = playerDirection = viewRadius = keyBindOpened = inspecting = music = playMusic = sfx = isLowResolution = start = disableMovement = playAudio = closeSetting = isSettingsOpen = currentMap = viewDiameter  = inventoryOpened = asciiMode= 0;
             var objectTypes = ["Bottom","Terrain","Item","Decor","Creature","Light","Top"];
             var keyBinds = (!localStorage.getItem("keyBinds"))?{"Inventory":"KeyE","Select":"Enter","Settings":"Escape","Attack":"Space","Movement":["ArrowRight","ArrowDown","ArrowLeft","ArrowUp","KeyD","KeyS","KeyA","KeyW"]}:(JSON.parse(localStorage.getItem("keyBinds")));
             if (localStorage.getItem("soundSettings")) {
@@ -579,6 +637,61 @@ if (file_exists("maps/Test.pkl")) {
             sendRequest();
             window.mobileCheck = function() {let check = false;(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);return check;};
             //if (mobileCheck()){toggleResolution()}
+            
+            function inspectPlayer() {
+                if (inspectingPlayer) {
+                    
+                } else {
+                    
+                }
+                inspectingPlayer = !inspectingPlayer;
+            }
+            function toggleActions(tileId) {
+                if (tileId == viewRadius+","+viewRadius && !inventoryOpened) {
+                    toggleInventory();
+                    return;
+                }
+                if (tileId == viewRadius+","+viewRadius && inspecting == tileId) {
+                    inspectTile(tileId)
+                    return;
+                }
+                if (document.getElementById(activeTile)) {
+                    document.getElementById(activeTile).classList.remove('inspecting');
+                }
+                var coordinates = tileId.split(",");
+
+                if ((!isAction || tileId != activeTile) && currentMap[coordinates[0]][coordinates[1]]['Bottom']['textureIndex'] != 8) {
+                    document.getElementById("action").style.height = "100px";
+                    var selectedTile = currentMap[coordinates[0]][coordinates[1]];
+                    document.getElementById(tileId).classList.add('inspecting');
+                    var inspectButton = "<button onclick = 'inspectTile(activeTile)'>Inspect</button>";
+                    var attackButton = "<button onclick = 'directionHandler([parseInt(activeTile.split(`,`)[0]),parseInt(activeTile.split(`,`)[1])])'>Attack</button>";
+                    var moveButton = "<button onclick = 'directionHandler([parseInt(activeTile.split(`,`)[0]),parseInt(activeTile.split(`,`)[1])])'>Move</button>";
+                    var interactButton = "<button onclick = 'directionHandler([parseInt(activeTile.split(`,`)[0]),parseInt(activeTile.split(`,`)[1])])'>Interact</button>";
+                    if (currentMap[coordinates[0]][coordinates[1]]['Bottom']['textureIndex'] != 8) {
+                    document.getElementById('action').innerHTML = inspectButton;
+                        if (document.getElementById(tileId).dataset.manhattan == 1) {
+                            document.getElementById('action').innerHTML += moveButton;
+                        }
+                    }
+
+                    activeTile = tileId;
+                    isAction = true;
+                    
+                    
+
+                    
+                } else {
+                    document.getElementById("action").style.height = "0px";
+                     document.getElementById('action').innerHTML = "";
+                    activeTile = 0;
+                    isAction = false;
+                }
+                
+               
+            }
+            
+            
             function createModal(content, id = "") {
                 disableMovement = true;
                 if (document.getElementById(`modal`)) {
@@ -673,9 +786,9 @@ if (file_exists("maps/Test.pkl")) {
                 scaleTextures();
                 document.getElementById("canvas").querySelectorAll('.tile').forEach((Item) => {
                     Item.addEventListener("click", () => {clickTile(Item.id);Item.blur();});
-                    Item.addEventListener("touchstart", () => {document.getElementById(Item.id).classList.add("inspectionLoad");this.touchStart = event.touches[0];clearTimeout(this.longPressTimeout);this.longPressTimeout = setTimeout(() => {inspectTile(Item.id);document.getElementById(Item.id).classList.remove("inspectionLoad");}, 1000);});
+                    Item.addEventListener("touchstart", () => {document.getElementById(Item.id).classList.add("inspectionLoad");this.touchStart = event.touches[0];clearTimeout(this.longPressTimeout);this.longPressTimeout = setTimeout(() => {toggleActions(Item.id);document.getElementById(Item.id).classList.remove("inspectionLoad");}, 1000);});
                     Item.addEventListener("touchend", () => {document.getElementById(Item.id).classList.remove("inspectionLoad");clearTimeout(this.longPressTimeout)});
-                    Item.addEventListener("contextmenu", function(ev){ev.preventDefault();inspectTile(Item.id)});
+                    Item.addEventListener("contextmenu", function(ev){ev.preventDefault();toggleActions(Item.id)});
                 });
             }
             var confirmationCoordinates = 0;
@@ -706,7 +819,13 @@ confirmationCoordinates = coordinates;
                 
                 return false
             }
-            function toggleInspect(tileId){
+            function toggleInspect(tileId, dontOpen = ""){
+                if (tileId == viewRadius+","+viewRadius && !inspecting) {
+                    toggleInventory();
+                    if (inventoryOpened) {
+                    return;
+                    }
+                }
                 var tile = currentMap[tileId.split(",")[0]][tileId.split(",")[1]]
                 if (inspecting) {
                     document.getElementById(inspecting).classList.remove('inspecting');
@@ -866,18 +985,66 @@ confirmationCoordinates = coordinates;
             var selectedItem = 0;
             function toggleInventory() {
                 if (!inventoryOpened) {
-                    document.getElementById('inventory').innerHTML = "";
+                    inspecting = 0
+                    if (isAction) {
+                    toggleActions(viewRadius+","+viewRadius)
+                    }
+                    
+                    var player =  currentMap[viewRadius][viewRadius]["Creature"];
                     var inventory = currentMap[viewRadius][viewRadius]["Creature"]["inventory"];
                     
+                    var inventoryBuffer = "";
+                    if (inventory.length > 0) {
+                    inventoryBuffer = "<br><p>Inventory</p><span>";
                     
                     for (var i = 0; i < inventory.length; i++) {
                         item = inventory[i];
-                         var buffer = "<span><p>"+item['name']+"</p><img src = '"+tileObjects[item['textureIndex']]['icon']+"'><p>"+item['amount']+"</p><button class = 'use' onclick = 'selectedItem = `Inventory:"+i+"`;toggleInventory();createMessage(`dialogue`,`Click On A Target`,2);'>Use</button></span>";
-                     
-                        document.getElementById('inventory').innerHTML += "<div>"+buffer+"</div>"
-                        
+                         inventoryBuffer += "<span><p>"+item['name']+"</p><img src = '"+tileObjects[item['textureIndex']]['icon']+"'><p>"+item['amount']+"</p><button class = 'use' onclick = 'selectedItem = `Inventory:"+i+"`;toggleInventory();createMessage(`dialogue`,`Click On A Target`,2);'>Use</button></span>";
+
+                    }
+                    inventoryBuffer += "</span>";
+                    }
+                     var equipmentBuffer = "";
+                    if (player['equipment'].length > 0) {
+                    equipmentBuffer = "<br><p>Equipment</p><span>";
+                    
+                    for (var i = 0; i < player['equipment'].length; i++) {
+                         equipmentBuffer += "<span><p>"+player['equipment'][i]['name']+"</p><img src = '"+tileObjects[player['equipment'][i]['textureIndex']]['icon']+"'><p>"+player['equipment'][i]['slot']+" | "+player['equipment'][i]['type']+"</p></span>";
+
+                    }
+                    equipmentBuffer += "</span>";
                     }
                     
+                    var abilityBuffer = "";
+                    if (player['abilities'].length > 0) {
+                    abilityBuffer = "<br><p>Abilities</p><span>";
+                    
+                    for (var i = 0; i < player['abilities'].length; i++) {
+                         abilityBuffer += "<span><p>"+player['abilities'][i]['name']+"</p><img src = '"+tileObjects[player['abilities'][i]['textureIndex']]['icon']+"'><button class = 'use' onclick = 'selectedItem = `abilities:"+i+"`;toggleInventory();createMessage(`dialogue`,`Click On A Target`,2);'>Use</button></span>";
+
+                    }
+                    abilityBuffer += "</span>";
+                    }
+                    
+                    var statusBuffer = "";
+                    if (player['status_effects'].length > 0) {
+                        statusBuffer += "<p id = 'status_effects'>" 
+                    for (var i = 0; i < player['status_effects'].length; i++) {
+                         statusBuffer += player['status_effects'][i]['status_type'];
+                    }
+                    statusBuffer += "</p><br>" 
+                    }
+                    
+                    
+                    
+                     var buffer = statusBuffer+'<p>'+player['name']+' | '+player['race']+'</p>';
+
+                    
+                    buffer += "<img src = '"+tileObjects[player['textureIndex']]['icon']+"'>";
+
+                     buffer += '<p>Score: '+player['score']+' LVL: '+player['level']+' XP: '+player['xp']+'/'+player['level']*20+'</p>';
+                     buffer += '<br><p>Cunning: '+player['cunning']+' Fitness: '+player['fitness']+' Magic: '+player['magic']+'</p>';
+                   document.getElementById('inventory').innerHTML += '<span id = "playerCard"><button onclick = "toggleInventory();">X</button>'+buffer+abilityBuffer+inventoryBuffer+equipmentBuffer+'<button onclick = "inspectTile(viewRadius+`,`+viewRadius, true);">Show More</button></span>';
                 }  else {
                      document.getElementById('inventory').innerHTML = "";
                 }
