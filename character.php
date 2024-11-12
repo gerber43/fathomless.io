c1f5c004b216a058e62328b02ef2f507fe051748
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-11-12 02:23:41 -0500

    Update character.php
    viewport

--- character.php
+++ character.php
@@ -13,6 +13,8 @@ if (file_exists("maps/".$_SESSION['uuid'].".pkl")) {
 ?>
 <html>
     <head>
+         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
         @import url('https://fonts.cdnfonts.com/css/8bit-wonder');
          * {
