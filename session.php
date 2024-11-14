349746a789a804be103674fcd6ff60b62b8adcb3
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-11-14 03:04:27 -0500

    Update session.php
    revert

--- session.php
+++ session.php
@@ -1,8 +1,9 @@
 <?php session_start();
     $uuid = $_SESSION['uuid']??null;
     $username = $_SESSION['username']??null;
+    /*Only use session variables to create access rules*/
     function session_required() {
-        if (!$uuid) {
+        if (!isset($_SESSION['uuid'])) {
             header("Location: https://fathomless.io/");
         }
     }
