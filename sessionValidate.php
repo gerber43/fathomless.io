976f5fdd4d18c9fafaf91457d31f56de0c7226f4
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-11-21 18:52:15 -0500

    Update sessionValidate.php
    added buy

--- sessionValidate.php
+++ sessionValidate.php
@@ -10,7 +10,20 @@ if (isset($_REQUEST['sendAttack']) && isset($uuid)) {
 
     
     
-} else if (isset($_REQUEST['levelUp']) && isset($uuid)) {
+} 
+else if (isset($_REQUEST['destroy']) && isset($uuid)) {
+    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".urlencode($uuid)."&destroy=destroy&attack=".urlencode($_REQUEST['destroy']));
+
+    
+    
+} 
+else if (isset($_REQUEST['buy']) && isset($uuid)) {
+    echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".urlencode($uuid)."&buy=".urlencode($_REQUEST['buy']));
+
+    
+    
+} 
+else if (isset($_REQUEST['levelUp']) && isset($uuid)) {
     echo file_get_contents("https://fathomless.io/cgi-bin/movement_api.py?uuid=".$uuid.(isset($_REQUEST['cunning'])?"&levelUp=levelUp&cunning=".urlencode($_REQUEST['cunning']):"").(isset($_REQUEST['magic'])?"&magic=".urlencode($_REQUEST['magic']):"").(isset($_REQUEST['fitness'])?"&fitness=".urlencode($_REQUEST['fitness']):""));
 
 }
