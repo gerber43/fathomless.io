1c04876ad37e92109255c13afeeea4d0e9dd8ea4
gerber43 <103010554+gerber43@users.noreply.github.com>
2024-11-21 20:06:54 -0500

    Update MasterGenerator.py
    added number of creatures

--- cgi-bin/MasterGenerator.py
+++ cgi-bin/MasterGenerator.py
@@ -2,10 +2,13 @@ def generateMap(algorithm_index,depth,player):
     depths = depth.split(",")
     depths[0] = int(depths[0])
     multipier = 1
+    number_creatures = 5
     if (depths[1] == "medium"):
         multipier = 2
+        number_creatures = 10
     if (depths[1] == "hard"):
         multipier = 3
+        number_creatures = 15
     if depths[0] == 0:
         algorithm_index = 2
     if algorithm_index == 0:
@@ -15,5 +18,5 @@ def generateMap(algorithm_index,depth,player):
     if algorithm_index == 2:
         from GenerateLevelZero import generateMap
     dimension = 10 + 2*int(depths[0]) if (depths[1] != "Test" and depths[0] != 22) else 20
-    final_grid = generateMap(dimension, dimension ,depth, multipier*(depths[0] + 1),player,depths[0]//multipier)
+    final_grid = generateMap(dimension, dimension ,depth, number_creatures,player,depths[0]//multipier)
     return final_grid
